91ff492a11b3fd48b95984e7c7bd3639ff7dcfe5
bazenga-glitch <72322047+bazenga-glitch@users.noreply.github.com>
2024-07-23 17:10:00 +0300

    Set up basic components and routing

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+// src/components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Fantasy Football App</h1>
+    </header>
+  );
+}
+
+export default Header;
